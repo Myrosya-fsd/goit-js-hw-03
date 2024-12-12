bd83c59f176ce54359414cfa10f1b98ebfb7f3e3
myroslava <murosja80@gmail.com>
2024-12-12 23:07:56 +0200

    hw-03

--- js/task-1.js
+++ js/task-1.js
@@ -25,3 +25,16 @@ console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
 console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
 
 //----
+
+function slugify(title) {
+  title = title.toLowerCase();
+  const slug = title.replace(/\s+/g, "-");
+  return slug;
+}
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+//--------
